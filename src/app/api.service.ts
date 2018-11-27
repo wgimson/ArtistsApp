3dd1ab6865b9d9204e00538eb7bcc18c080498d8
wgimson <wgimson@gmail.com>
2018-11-27 13:42:34 -0500

    api calls complete; charts added;

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -10,11 +10,36 @@ const httpOptions = {
 export class ApiService {
 
   host = 'https://api.nextbigsound.com';
+  accessToken = 'eb74a82009cbc53c9b44866743633f9d';
 
   constructor(private http: HttpClient) { }
 
-  getArtists() {
-    const path = '/search/v1/artists/?query=kanye+west&limit=10&access_token=eb74a82009cbc53c9b44866743633f9d';
+  getArtists(artist, limit) {
+    const path = `/search/v1/artists/?query=${artist}&limit=${limit}&access_token=${this.accessToken}`;
+    const url = this.host + path;
+    return this.http.get(url);
+  }
+
+  getEvents(artistId, startDay, endDay) {
+    const path = `/events/v1/entity/${artistId}?start=${startDay}&end=${endDay}&access_token=${this.accessToken}`;
+    const url = this.host + path;
+    return this.http.get(url);
+  }
+
+  getTimeSeries(artistId, metrics, startDate, endDate) {
+    const path = `/artists/${artistId}/data?metricIds=${metrics}&startDate=${startDate}&endDate=${endDate}&timeseries=totals,deltas&access_token=${this.accessToken}`;
+    const url = this.host + path;
+    return this.http.get(url);
+  }
+
+  getAllMetrics() {
+    const path = `https://api.nextbigsound.com/metrics?fields=items.*&access_token=${this.accessToken}`;
+    const url = this.host + path;
+    return this.http.get(url);
+  }
+
+  getMetric(metricId) {
+    const path = `https://api.nextbigsound.com/metrics/${metricId}?&access_token=${this.accessToken}`;
     const url = this.host + path;
     return this.http.get(url);
   }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as d3 from 'd3';
+
 import { ApiService } from './api.service';
 
 @Component({
@@ -10,15 +12,94 @@ import { ApiService } from './api.service';
 export class AppComponent implements OnInit {
   title = 'app';
   kanye: any;
+  events: any;
+  timeseries: any;
+  allMetrics: any;
+  twitterMetric: any;
 
   constructor(private apiService: ApiService) {}
 
   ngOnInit() {
-    this.apiService.getArtists()
+    // TEST ALL APIS
+    // GET ARTIST
+    this.apiService.getArtists('Kanye West', 10)
       .subscribe(
-        data => { this.kanye = data; },
+        data => { this.kanye = data['artists'][0]; console.log(this.kanye); },
         err => console.log(err),
-        () => console.log('done loading artist data')
+      // GET ARTIST EVENTS
+        () => {
+          this.getArtistEvents(this.kanye.id, 17167, 17531);
+          // GET ARTISTS TIMESERIES
+          this.getArtistTimeSeries(this.kanye.id, [28, 247], '2017-01-01', '2017-12-31');
+          console.log('done loading artist data');
+        }
+      );
+
+      this.createChart();
+  }
+
+  createChart() {
+    const w = 400, h = 250;
+    const padding = 4;
+    const data = [50, 100, 150, 200, 250, 130, 210, 30, 170];
+
+    const svg = d3.select('body')
+                .append('svg')
+                .attr('width', w)
+                .attr('height', h);
+
+    svg.selectAll('rect')
+      .data(data)
+      .enter()
+        .append('rect')
+        .attr('x', (d, i) => i * (w / data.length))
+        .attr('y', d => h - d)
+        .attr('width', w / data.length - padding)
+        .attr('height', d => d)
+        .attr('fill', 'green');
+
+        svg.selectAll('text')
+           .data(data)
+           .enter()
+              .append('text')
+              .text(d => d)
+              .attr('x', (d, i) => i * (w / data.length) + (w / data.length - padding) / 2)
+              .attr('y', (d) => h - d + 20);
+  }
+
+  getArtistEvents(artistId, startDays, endDays) {
+     this.apiService.getEvents(artistId, startDays, endDays)
+        .subscribe(
+          data => { this.events = data; console.log(this.events); },
+          err => console.log(err),
+          () => console.log('done loading event data')
+        );
+  }
+
+  getArtistTimeSeries(id, metrics, startDate, endDate) {
+      this.apiService.getTimeSeries(id, metrics, startDate, endDate)
+      .subscribe(
+        data => { this.timeseries = data; console.log(this.timeseries); },
+        err => console.log(err),
+        () => console.log('done loading timeseries data')
+      );
+  }
+
+  getAllMetrics() {
+    this.apiService.getAllMetrics()
+      .subscribe(
+        data => { this.allMetrics = data; console.log(this.allMetrics); },
+        err => console.log(err),
+        () => console.log('done loading all metrics data')
+      );
+  }
+
+  getMetric(id) {
+    this.apiService.getMetric(id)
+      .subscribe(
+        data => { this.twitterMetric = data; console.log(this.twitterMetric); },
+        err => console.log(err),
+        () => console.log('done loading twitter metric data')
       );
   }
 }
